e61f768c2e6bee38d2b63e31f2e84cfbf12027fd
Chandrahasa Pranava <81606179+rh-cpranava@users.noreply.github.com>
2022-06-08 08:53:08 +0800

    Create flask_redirect.py

---
+++ flask_redirect.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+"""
+flask_redirect: Redirects requests to appropriate URL
+"""
+import os
+import requests
+from flask import Flask, redirect  # From module flask import class Flask
+app = Flask(__name__)    # Construct an instance of Flask class for our webapp
+
+@app.route('/redirect/<redirect_url>')
+def redirect(redirect_url):
+    if "BASE_URL" in os.environ:
+        base_url = os.environ.get('BASE_URL')
+        final_url = redirect_url + '.' + base_url
+        check_page_validity = requests.get(final_url)
+        if request.status_code == 200:
+            redirect(base_url)
+        else:
+            return render_template('error.html')
+    if "BASE_URL" not in os.environ:
+        return render_template('error.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
