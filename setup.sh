80f3cd7e9feaf96aae9236a8a93af8107da62392
quicklab <quicklab@upi-0.sharedocp4upi48.lab.upshift.rdu2.redhat.com>
2022-06-07 23:49:02 -0400

    Added logic for delete deployment

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+oc delete deploy flask-redirect
 oc new-app --docker-image=quay.io/cpranava0/flask-redirect:main
 oc set env deploy/flask-redirect BASE_URL=redhat.com
 oc expose svc flask-redirect
