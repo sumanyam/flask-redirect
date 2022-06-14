6540519e26726baed79ce274e8fc6763de09cb65
Chandrahasa Pranava <81606179+rh-cpranava@users.noreply.github.com>
2022-06-14 12:11:12 +0800

    Update flask_redirect.py

--- flask_redirect.py
+++ flask_redirect.py
@@ -7,6 +7,42 @@ import requests
 from flask import Flask, redirect, render_template  # From module flask import class Flask
 app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
+@app.route('/')
+def base_function():
+    if "POD_NAME" in os.environ:
+        pod_name = os.environ.get('POD_NAME')
+    else:
+        return render_template('error.html')
+    
+    if "NODE_NAME" in os.environ:
+        node_name = os.environ.get('NODE_NAME')
+    else:
+        return render_template('error.html')
+    
+    if "POD_NAMESPACE" in os.environ:
+        namespace_name = os.environ.get('POD_NAMESPACE')
+    else:
+        return render_template('error.html')
+    
+    if "POD_IP" in os.environ:
+        pod_ip = os.environ.get('POD_IP')
+    else:
+        return render_template('error.html')
+    
+    if "POD_SERVICE_ACCOUNT" in os.environ:
+        pod_service_account = os.environ.get('POD_SERVICE_ACCOUNT')
+     else:
+        return render_template('error.html')
+    
+    return {
+        "pod_name": pod_name,
+        "node_name": node_name,
+        "namespace_name": namespace_name,
+        "pod_ip": pod_ip,
+        "pod_service_account": pod_service_account,
+    }
+
+
 @app.route('/redirection/<redirect_url>')
 def redirection(redirect_url):
     if "BASE_URL" in os.environ:
