34def0b7056a7e930bd7ec57112c5fd9912144e9
GTPE Student <lab-user@bastion.fsl8m.internal>
2022-06-14 08:53:37 +0000

    Added commit details

---
+++ delete.sh
@@ -0,0 +1,4 @@
+oc delete deploy flask-redirect
+oc delete is flask-redirect
+oc delete svc flask-redirect
+
--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 oc delete deploy flask-redirect
 oc new-app --docker-image=quay.io/cpranava0/flask-redirect:main
 oc set env deploy/flask-redirect BASE_URL=redhat.com
+oc set env deploy/flash-redirect 
 oc expose svc flask-redirect
